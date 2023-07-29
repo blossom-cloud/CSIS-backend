f656fe6139ab24a18cffa55d5999e4f7856a083d
Blossom Goyal <b1@student.douglascollege.ca>
2023-07-28 17:49:44 -0700

    cors update

--- src/main/java/com/example/project_management_tool/web/BacklogController.java
+++ src/main/java/com/example/project_management_tool/web/BacklogController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/backlog")
-@CrossOrigin(origins = " * ", allowedHeaders = " * ")
+@CrossOrigin
 public class BacklogController {
 
     @Autowired
--- src/main/java/com/example/project_management_tool/web/ProjectController.java
+++ src/main/java/com/example/project_management_tool/web/ProjectController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 //Controller layer to pass all CRUD operations to service layer
 @RestController
 @RequestMapping("/api/project")
-@CrossOrigin(origins = " * ", allowedHeaders = " * ")
+@CrossOrigin
 public class ProjectController {
 
     @Autowired
